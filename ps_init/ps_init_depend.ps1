10d850bbd472213cbb335fc4c605a5422b4aa18f
AnthonyK213 <tjjzqwh@163.com>
2021-01-22 14:59:59 +0800

    210122

--- ps_init/ps_init_depend.ps1
+++ ps_init/ps_init_depend.ps1
@@ -17,5 +17,5 @@ function vims  { nvim.exe -S $args }
 function gvims { nvim-qt.exe -- -S $args }
 function vim.  { nvim.exe $executionContext.SessionState.Path.CurrentLocation }
 function gvim. { nvim-qt.exe $executionContext.SessionState.Path.CurrentLocation }
-function nano  { nvim.exe --cmd "let g:init_src='nano'" $args }
-function gnano { nvim-qt.exe -qwindowgeometry 720x900 -- --cmd "let g:init_src='nano'" $args }
+function nano  { nvim.exe --noplugin --cmd "let g:init_src='nano'" $args }
+function gnano { nvim-qt.exe -qwindowgeometry 720x900 -- --noplugin --cmd "let g:init_src='nano'" $args }

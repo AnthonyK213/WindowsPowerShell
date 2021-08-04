7814f7ca9592a731dd38bd6b2c7c573a7d790e81
AnthonyK213 <tjjzqwh@163.com>
2021-08-04 09:53:20 +0800

    fix

--- init/depend.ps1
+++ init/depend.ps1
@@ -17,7 +17,7 @@ function gvims { nvim-qt.exe -- -S $args }
 function vim.  { nvim.exe $executionContext.SessionState.Path.CurrentLocation }
 function gvim. { nvim-qt.exe $executionContext.SessionState.Path.CurrentLocation }
 function nano  { nvim.exe --cmd "let g:init_src='nano'" $args }
-function gnano { nvim-qt.exe -qwindowgeometry 720x900 -- --cmd "let g:init_src='nano'" $args }
+function gnano { nvim-qt.exe -qwindowgeometry 720x900 -- --cmd "let g:nvim_init_src='nano'" $args }
 
 # Rust
 $RUSTUP_DIST_SERVER = "https://mirrors.tuna.tsinghua.edu.cn/rustup"

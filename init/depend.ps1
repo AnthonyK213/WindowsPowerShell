31a04f6dbcb294df6de6d3d190b79789cd2e7de6
AnthonyK213 <tjjzqwh@163.com>
2022-08-09 11:05:34 +0800

    clean

--- init/depend.ps1
+++ init/depend.ps1
@@ -1,14 +1,7 @@
 ## Frequently used app
-Set-Alias vi       nvim.exe
-Set-Alias vim      nvim.exe
-Set-Alias emx      runemacs.exe
-Set-Alias bld      blender.exe
-
-# Emacs(Win) really sucks!
-function enw {
-    chcp 936
-    emacs.exe -nw $args
-}
+Set-Alias vi  nvim.exe
+Set-Alias vim nvim.exe
+Set-Alias bld blender.exe
 
 # Neovim
 function gvim  { nvim-qt.exe -- }
@@ -17,7 +10,7 @@ function gvims { nvim-qt.exe -- -S $args }
 function vim.  { nvim.exe $executionContext.SessionState.Path.CurrentLocation }
 function gvim. { nvim-qt.exe $executionContext.SessionState.Path.CurrentLocation }
 function nano  { nvim.exe --cmd "let g:nvim_init_src='nano'" $args }
-function gnano { nvim-qt.exe -qwindowgeometry 720x900 -- --cmd "let g:nvim_init_src='nano'" $args }
+function gnano { nvim-qt.exe -qwindowgeometry 1280x800 -- --cmd "let g:nvim_init_src='nano'" $args }
 
 # Rust
 $RUSTUP_DIST_SERVER = "https://mirrors.tuna.tsinghua.edu.cn/rustup"
